cba0ea399414309518605c1a5c0180c3039a89e0
Christiaan de Ridder <ik@christiaanderidder.nl>
2014-04-15 15:35:48 +0200

    * Use tenant instead of the api key for the tracking script url

--- app/code/community/Tritac/ChannelEngine/Block/Head.php
+++ app/code/community/Tritac/ChannelEngine/Block/Head.php
@@ -3,6 +3,6 @@ class Tritac_ChannelEngine_Block_Head extends Mage_Core_Block_Template
 {
     public function getAccountName() {
         $config = Mage::helper('channelengine')->getGeneralConfig();
-        return $config['api_key'];
+        return $config['tenant'];
     }
 }
