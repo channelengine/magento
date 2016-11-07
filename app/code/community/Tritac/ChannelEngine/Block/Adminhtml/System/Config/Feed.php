802865cc206e9f3c4a42b8759bb1adf73e6c4981
Christiaan de Ridder <ik@christiaanderidder.nl>
2016-11-07 15:40:46 +0100

    Fix button html

--- app/code/community/Tritac/ChannelEngine/Block/Adminhtml/System/Config/Feed.php
+++ app/code/community/Tritac/ChannelEngine/Block/Adminhtml/System/Config/Feed.php
@@ -7,47 +7,49 @@
  */
 class Tritac_ChannelEngine_Block_Adminhtml_System_Config_Feed extends Mage_Adminhtml_Block_System_Config_Form_Field
 {
-    /**
-     * Set template block template
-     *
-     * @return Tritac_ChannelEngine_Block_Adminhtml_System_Config_Feed
+    /*
+     * Set template
      */
-    protected function _prepareLayout()
+    protected function _construct()
     {
-        parent::_prepareLayout();
-        if (!$this->getTemplate()) {
-            $this->setTemplate('channelengine/system/config/feed/generate_button.phtml');
-        }
-        return $this;
+        parent::_construct();
+        $this->setTemplate('channelengine/system/config/feed/generate_button.phtml');
     }
 
     /**
-     * Disable website scope
+     * Return element html
      *
-     * @param Varien_Data_Form_Element_Abstract $element
-     * @return string
-     */
-    public function render(Varien_Data_Form_Element_Abstract $element)
-    {
-        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
-        return parent::render($element);
-    }
-
-    /**
-     * Get the button and scripts contents
-     *
-     * @param Varien_Data_Form_Element_Abstract $element
+     * @param  Varien_Data_Form_Element_Abstract $element
      * @return string
      */
     protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
     {
-        $originalData = $element->getOriginalData();
-        $this->addData(array(
-            'button_label' => Mage::helper('channelengine')->__($originalData['button_label']),
-            'html_id' => $element->getHtmlId(),
-            'ajax_url' => Mage::getSingleton('adminhtml/url')->getUrl('channelengine/adminhtml_generate/ajax')
-        ));
-
         return $this->_toHtml();
+    }
+
+    /**
+     * Return ajax url for button
+     *
+     * @return string
+     */
+    public function getAjaxUrl()
+    {
+        return Mage::helper('adminhtml')->getUrl('channelengine/adminhtml_generate/ajax');
+    }
+
+    /**
+     * Generate button html
+     *
+     * @return string
+     */
+    public function getButtonHtml()
+    {
+        $button = $this->getLayout()->createBlock('adminhtml/widget_button')->setData(array(
+            'id'       => 'channelengine_generate_feed_button',
+            'label'    => $this->helper('channelengine')->__('Generate Feed'),
+            'onclick'  => 'javascript:generateFeed(); return false;'
+        ));
+ 
+        return $button->toHtml();
     }
 }
