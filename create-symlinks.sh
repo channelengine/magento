d2c693b810ee61bbdaf131e1f009140134145306
Ermst4r <erm@live.nl>
2018-10-15 15:52:50 +0200

    added symlinks for linux

---
+++ create-symlinks.sh
@@ -0,0 +1,6 @@
+ln -s  /USER_DIR/ce-magento/app/etc/modules/Tritac_ChannelEngine.xml /USER_DIR/magento1/app/etc/modules/Tritac_ChannelEngine.xml
+ln -s  /USER_DIR/ce-magento/app/design/frontend/base/default/template/channelengine /USER_DIR/magento1/app/design/frontend/base/default/template/channelengine
+ln -s  /USER_DIR/ce-magento/app/code/community/Tritac  /USER_DIR/magento1/app/code/community/Tritac
+ln -s  /USER_DIR/ce-magento/app/design/adminhtml/default/default/template/channelengine /USER_DIR/magento1/app/design/adminhtml/default/default/template/channelengine
+ln -s  /USER_DIR/ce-magento/app/design/frontend/base/default/layout/channelengine.xml  /USER_DIR/magento1/app/design/frontend/base/default/layout/channelengine.xml
+ln -s  /USER_DIR/ce-magento/lib/ChannelEngine  /USER_DIR/magento1/lib/ChannelEngine
