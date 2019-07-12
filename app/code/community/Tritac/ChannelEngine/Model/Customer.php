f0d0087d9745755104e90572ce41d8e7d7e9b22c
Christiaan de Ridder <ik@christiaanderidder.nl>
2019-07-12 12:45:42 +0200

    Fix syntax error

--- app/code/community/Tritac/ChannelEngine/Model/Customer.php
+++ app/code/community/Tritac/ChannelEngine/Model/Customer.php
@@ -55,7 +55,7 @@ class Tritac_ChannelEngine_Model_Customer  extends  Tritac_ChannelEngine_Model_B
     /**
      * @param mixed $shipping_data
      */
-    public function setShippingData($shippingAddress,$order)
+    public function setShippingData($shippingAddress, $order)
     {
         $this->shipping_data =  array(
             'company'       => $shippingAddress->getCompanyName(),
@@ -68,7 +68,7 @@ class Tritac_ChannelEngine_Model_Customer  extends  Tritac_ChannelEngine_Model_B
             'city'          => $shippingAddress->getCity(),
             'street'        =>
                 $shippingAddress->getStreetName()."\n".
-                $shippingAddress->getHouseNr()." "
+                $shippingAddress->getHouseNr()." ".
                 $shippingAddress->getHouseNrAddition()
         );
     }
